70125d4450815b4a4d00f8881781ccef8be40a6a
sajad mahmoodnasab <mahmoodnasabsajad@gmail.com>
2026-02-08 11:28:07 +0330

    Add live search functionality for products
    - Implements a real-time search on product items.
    - Filters products as the user types in the search input.
    - Converts both input and product names to lowercase for case-insensitive matching.
    - Shows matching products and hides non-matching ones dynamically.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 'use strict';
+const searchInput = document.getElementById('search-input');
+const products = document.querySelectorAll('.prouduct-item');
+const searchHandler = event => {
+  const searchValue = event.target.value.toLowerCase().trim();
+    products.forEach(product => {
+     
+    const prouctsName = product.children[1].innerText.toLowerCase();
+        if (prouctsName.includes(searchValue)) {
+       product.style.display='block'
+        } else {
+            product.style.display='none'
+    }
+  });
+};
+searchInput.addEventListener('keyup', searchHandler);
